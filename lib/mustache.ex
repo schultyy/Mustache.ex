de08effe5097538709e1b3c1319341a30d45c815
Jan Schulte <hello@unexpected-co.de>
2015-04-04 23:33:14 +0200

    Compile unadorned template

--- lib/mustache.ex
+++ lib/mustache.ex
@@ -1,5 +1,22 @@
 defmodule Mustache do
-  def render(text) do
-    text
+  def render(template, data \\ %{}) do
+    {:ok, pattern} = Regex.compile("\{\{\\w+\}\}")
+    scans = Regex.scan(pattern, template) |> List.flatten
+    case scans do
+      [] -> template
+      _ ->
+        variable = List.first(scans) |> clean |> String.to_atom
+        value = data[variable]
+        if value == nil do
+          template
+        else
+          String.replace(template, "{{#{variable}}}", value)
+        end
+    end
+  end
+
+  defp clean(str) do
+    String.replace(str, "{{", "")
+      |> String.replace("}}", "")
   end
 end
--- test/mustache_test.exs
+++ test/mustache_test.exs
@@ -4,4 +4,8 @@ defmodule MustacheTest do
   test "mustache free template should render as it is" do
     assert Mustache.render("Hello from {Mustache}!\n")  == "Hello from {Mustache}!\n"
   end
+
+  test "Unadorned tags should interpolate content into the template." do
+    assert Mustache.render("Hello, {{subject}}!\n", %{subject: "world"}) == "Hello, world!\n"
+  end
 end
