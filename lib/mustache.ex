47cacde311925d45ee65f0e6c330e4b0efe97892
Jan Schulte <hello@unexpected-co.de>
2015-04-05 00:02:02 +0200

    HTML escape values

--- lib/mustache.ex
+++ lib/mustache.ex
@@ -6,13 +6,20 @@ defmodule Mustache do
       [] -> template
       _ ->
         variable = List.first(scans) |> clean |> String.to_atom
-        value = data[variable]
+        value = data[variable] |> escape
         if value == nil do
           template
         else
           String.replace(template, "{{#{variable}}}", value)
         end
     end
+  end
+
+  defp escape(non_escaped) do
+    forbidden = [{"&", "&amp;"}, {"<","&lt;" }, {">", "&gt;"}, {"\"", "&quot;"}]
+    Enum.reduce(forbidden, non_escaped, fn (x, str) ->
+      String.replace(str, elem(x, 0), elem(x, 1))
+    end)
   end
 
   defp clean(str) do
--- test/mustache_test.exs
+++ test/mustache_test.exs
@@ -8,4 +8,8 @@ defmodule MustacheTest do
   test "Unadorned tags should interpolate content into the template." do
     assert Mustache.render("Hello, {{subject}}!\n", %{subject: "world"}) == "Hello, world!\n"
   end
+
+  test "Basic interpolation should be HTML escaped." do
+    assert Mustache.render("These characters should be HTML escaped: {{forbidden}}\n", %{forbidden: "& \" < >"}) == "These characters should be HTML escaped: &amp; &quot; &lt; &gt;\n"
+  end
 end
