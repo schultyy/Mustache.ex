fa091bc19055f70bb0a33aaa0fa09520add11036
Jan Schulte <hello@unexpected-co.de>
2015-04-05 21:42:21 +0200

    remove unused code

--- lib/mustache.ex
+++ lib/mustache.ex
@@ -49,22 +49,6 @@ defmodule Mustache do
     Regex.compile!("\{\{\{\\w+\}\}\}")
   end
 
-  # def render(template, data \\ %{}) do
-  #   {:ok, pattern} = Regex.compile("\{\{\\w+\}\}")
-  #   scans = Regex.scan(pattern, template) |> List.flatten
-  #   case scans do
-  #     [] -> template
-  #     _ ->
-  #       variable = List.first(scans) |> clean |> String.to_atom
-  #       value = data[variable] |> escape
-  #       if value == nil do
-  #         template
-  #       else
-  #         String.replace(template, "{{#{variable}}}", value)
-  #       end
-  #   end
-  # end
-
   defp escape(non_escaped) do
     forbidden = [{"&", "&amp;"}, {"<","&lt;" }, {">", "&gt;"}, {"\"", "&quot;"}]
     Enum.reduce(forbidden, non_escaped, fn (x, str) ->
