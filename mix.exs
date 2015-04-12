1c3bf70863d3e1948ea6c1a76e92d62ab93c6ed6
Jan Schulte <hello@unexpected-co.de>
2015-04-13 00:06:36 +0200

    increase version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Mustache.Mixfile do
 
   def project do
     [app: :mustache,
-     version: "0.0.1",
+     version: "0.0.2",
      elixir: "~> 1.0",
      package: package,
      description: "Mustache templates for Elixir",
