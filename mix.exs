9974e775d55925389c2fbc0cce9442c5ad63302c
Jan Schulte <jan@work-with-jan.com>
2021-08-08 11:16:06 -0400

    Bump version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Mustache.Mixfile do
 
   def project do
     [app: :mustache,
-     version: "0.3.1",
+     version: "0.4.0",
      elixir: "~> 1.0",
      package: package(),
      description: "Mustache templates for Elixir",
