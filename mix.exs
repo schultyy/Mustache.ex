22a123e730de6d5ff9fc5f599a7390ae6c840be7
Jan Schulte <jan@work-with-jan.com>
2021-08-08 11:30:55 -0400

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -10,6 +10,12 @@ defmodule Mustache.Mixfile do
      deps: deps()]
   end
 
+  defp description do
+    """
+    A Mustache Implementation for Elixir
+    """
+  end
+
   # Configuration for the OTP application
   #
   # Type `mix help compile.app` for more information
@@ -28,7 +34,8 @@ defmodule Mustache.Mixfile do
   # Type `mix help deps` for more examples and options
   defp deps do
     [
-      {:ex_doc, ">= 0.0.0", only: :dev}
+      {:ex_doc, ">= 0.0.0", only: :dev},
+      {:earmark, ">= 0.0.0", only: :dev}
     ]
   end
 
